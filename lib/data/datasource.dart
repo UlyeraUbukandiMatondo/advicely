8fb3d2f9275d2929a5257e0be31e38fd679323f1
matondoulyeraubukando <lucmatondoubukandi@gmail.com>
2026-02-26 11:55:02 +0200

    Update

---
+++ lib/data/datasource.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:advicely/data/model.dart';
+import 'package:http/http.dart' as http;
+
+Future<Conseil> genererConseil() async {
+  final client = http.Client();
+  final uri = "https://api.api-ninjas.com/v1/advice";
+  final cle = "";
+  final reponse = await client.get(Uri.parse(uri), headers: {"X-Api-Key": cle});
+  final json =
+      jsonDecode(utf8.decode(reponse.bodyBytes))
+          as Map; //L'api ne retourne pas une liste dans ce cas present
+
+  return Conseil.fromJSON(json);
+}
---
+++ lib/data/model.dart
@@ -0,0 +1,9 @@
+class Conseil {
+  final String conseilTexte;
+
+  Conseil({this.conseilTexte = ""});
+
+  factory Conseil.fromJSON(Map<dynamic, dynamic> json) {
+    return Conseil(conseilTexte: json["advice"] as String);
+  }
+}
